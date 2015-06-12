777d618f8b73d4de9079df47828e91080afdf674
Gigi Heins <gigiheins@gmail.com>
2015-06-11 20:15:37 -0400

    super

--- web/boopstrap.php
+++ web/boopstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 $website_settings = [
+    'home_title' => 'MARKUS HEINS',
     'title' => 'MARKUS HEINS,LLC',
     'page_title' => 'Best of',
+    'page_footer' => 'Copyright &copy Markus Heins %s',
 ];
