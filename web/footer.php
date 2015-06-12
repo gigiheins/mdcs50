7e9f954ec056db4c5cc777a988a4b3cf577f23ad
gigiheins <gigiheins@gmail.com>
2015-06-11 22:17:31 -0400

    uper super

---
+++ web/footer.php
@@ -0,0 +1,26 @@
+<?php
+require_once'boopstrap.php';
+?>
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p><?= sprintf($website_settings['page_footer'], date('Y')); ?></p>
+        </div>
+    </div>
+</footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
