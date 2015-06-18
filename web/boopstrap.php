6f942a8aa84c195abe37ef0fec6cb2c3ce142c83
gigiheins <gigiheins@gmail.com>
2015-06-18 17:23:23 -0400

    super

--- web/boopstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$website_settings = [
-    'home_title' => 'MARKUS HEINS',
-    'title' => 'MARKUS HEINS,LLC',
-    'page_title' => 'Best of',
-    'page_footer' => 'Copyright &copy Markus Heins %s',
-];
---
+++ web/bootstrap.php
@@ -0,0 +1,59 @@
+<?php
+$website_settings = [
+    'title' => 'Pirate Islands, LLC',
+    'home' => 'Pirate Life',
+    'page_title' => 'Ship wreak',
+    'page_description' => 'List of all my ship wreaks',
+    'copyright' => 'Copyright &copy; Pirate Islands %s',
+];
+$portfolio_collection = [
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+];
+$portfolio_collection = array_merge(
+    $portfolio_collection,
+    $portfolio_collection,
+    $portfolio_collection
+);
+$carousels = [
+    'about' => [
+    ],
+    'home' => [
+    ],
+];
+$about_carousel = [
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide One',
+        'caption' => 'Caption 1',
+    ],
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide Two',
+        'caption' => 'Caption 2',
+    ],
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide Three',
+        'caption' => 'Caption 3',
+    ],
+];
